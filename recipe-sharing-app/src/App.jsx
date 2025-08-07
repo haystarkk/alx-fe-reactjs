00f5fbf301d31578d219efbf99b0b8fa935dae3e
Michael <165153585+haystarkk@users.noreply.github.com>
2025-08-07 13:26:24 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,31 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RecipeList from './components/RecipeList';
-import RecipeDetails from './components/RecipeDetails';
-import AddRecipeForm from './components/AddRecipeForm';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
-import NavBar from './components/NavBar';
+import RecipeList from './components/RecipeList';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <NavBar />
-        
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={
-              <>
-                <RecommendationsList />
-                <RecipeList />
-              </>
-            } />
-            <Route path="/favorites" element={<FavoritesList />} />
-            <Route path="/recipes/:id" element={<RecipeDetails />} />
-            <Route path="/add" element={<AddRecipeForm />} />
-          </Routes>
-        </main>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/favorites">Favorites</NavLink>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={
+            <>
+              <RecommendationsList />
+              <RecipeList />
+            </>
+          } />
+          <Route path="/favorites" element={<FavoritesList />} />
+        </Routes>
       </div>
     </Router>
   );
