597797ebe7a30837e9d9c68009f3bb9bc5cbdeb6
Michael <165153585+haystarkk@users.noreply.github.com>
2025-08-07 09:44:20 +0300

    Create App.jsx

---
+++ github-user-search/App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search App</h1>
+      <p>Start typing to search GitHub users...</p>
+    </div>
+  );
+}
+
+export default App;
