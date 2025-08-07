5921a252084cf6f0ddfa2b687f5d31165bff4dc2
Michael <165153585+haystarkk@users.noreply.github.com>
2025-08-07 10:03:34 +0300

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,28 +1,13 @@
-import { useState } from 'react'
-import { searchUsers } from './services/githubAPI'
-import SearchBar from './components/SearchBar'
-import UserCard from './components/UserCard'
-import './index.css'
+import SearchUser from './components/SearchUser';
+import './index.css';
 
 function App() {
-  const [users, setUsers] = useState([])
-
-  const handleSearch = async (query) => {
-    const results = await searchUsers(query)
-    setUsers(results)
-  }
-
   return (
     <div className="app">
       <h1>GitHub User Search</h1>
-      <SearchBar onSearch={handleSearch} />
-      <div className="users-grid">
-        {users.map((user) => (
-          <UserCard key={user.id} user={user} />
-        ))}
-      </div>
+      <SearchUser />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
