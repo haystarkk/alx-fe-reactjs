ce8657ef8e3a226f71216db99f9e6860053afdb6
Michael <165153585+haystarkk@users.noreply.github.com>
2025-08-07 10:17:44 +0300

    Update and rename githubAPI.js to githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -7,8 +7,6 @@ export const fetchUserData = async (username) => {
     );
     return { data: response.data, error: null };
   } catch (error) {
-    return { data: null, error: error.response?.status === 404 
-      ? "User not found" 
-      : "An error occurred" };
+    return { data: null, error: "Looks like we can't find the user" };
   }
 };
