15770b8b5a3aa73121b9c2cdfb3c0f5aad983c90
Michael <165153585+haystarkk@users.noreply.github.com>
2025-08-17 22:35:20 +0300

    Update tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -2,8 +2,8 @@
 export default {
   content: [
     "./index.html",
-    "./public/index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
     extend: {},
